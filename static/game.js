8b386279eeb179667d75f273f74b7971d1b536e3
lupakd <daniel.lupis@gmail.com>
2019-03-05 21:35:54 +0100

    random popups for set interval, reset to default 'image' after set timeout.
    missed hits registered
    good hits registered

--- static/game.js
+++ static/game.js
@@ -1,14 +1,37 @@
 
 
-function getClicks() {
-    let table = document.querySelectorAll('.col-md-1');
+function timed_popups (table) {
 
+    setInterval(function () {
+    let current_cell = table[Math.floor(Math.random()*25)];
+        current_cell.textContent = 'KAL';
+        setTimeout(function () {current_cell.textContent = 'O'}, 1500);
+        }, 2000);
+    }
+
+
+function checkForClicks(table) {
 
     for (let cell of table) {
         cell.addEventListener('click', function () {
-            cell.textContent = 'KAL';
+            // ternary ops here!
+            if (cell.textContent === 'KAL') {
+                cell.textContent = 'XX';
+            } else {
+                cell.textContent = 'FAIL';
+            }
+            setTimeout(function () {
+                cell.textContent = 'O'
+            }, 800);
         });
     }
 }
 
-getClicks();
+
+function init() {
+    let table = document.querySelectorAll('.col-md-1');
+    timed_popups(table);
+    checkForClicks(table);
+}
+
+init();
