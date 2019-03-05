7276a2914bcf3eaf615c6bd57c6a014603200965
lupakd <daniel.lupis@gmail.com>
2019-03-05 09:24:20 +0100

    init

---
+++ server.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def start_page():
+    return render_template('whack-a-mole.html')
+
+
+if __name__ == '__main__':
+    app.run(Debug=True)
+
+
