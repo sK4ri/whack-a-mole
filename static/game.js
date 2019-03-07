fd11d62fcdd88d005a8d36fe9c72658124079b82
sK4ri <skari.simon@gmail.com>
2019-03-07 17:46:45 +0100

    Before release

--- static/game.js
+++ static/game.js
@@ -1,5 +1,3 @@
-
-
 function endGame() {
     document.getElementsByClassName('container')[0].style.display = 'none';
     document.getElementsByClassName('life')[0].style.display = 'none';
@@ -7,44 +5,35 @@ function endGame() {
 }
 
 
-function checkState (life, score) {
-
+function checkState(life, score) {
     refreshScores(life, score);
     if (life < 1 || score < 0) {
         alert('GAME OVER!');
-        //death.play()
     } else if (score >= 50) {
         alert('YOU WON!');
-        alert('GAME OVER!');
-        endGame();
-    } else if (score === 50) {
-        alert('Level 2.');
-    } else if (score >= 100) {
-        alert('YOU WIN');
         endGame();
     }
 }
 
 
-function refreshScores (life, score) {
-
+function refreshScores(life, score) {
     document.querySelector('.life').innerHTML = `<div id="life">${'Life: ' + life}</div>`;
-    document.querySelector('.scoring-system').innerHTML = `<div id="scoring-system">${'Score: '+ score}</div>`;
+    document.querySelector('.scoring-system').innerHTML = `<div id="scoring-system">${'Score: ' + score}</div>`;
 }
 
 
-function timedPopups (table, mole, field, interval) {
-
+function timedPopups(table, mole, field, interval) {
     setInterval(function () {
-    let currentCell = table[Math.floor(Math.random()*table.length)];
+        let currentCell = table[Math.floor(Math.random() * table.length)];
         currentCell.innerHTML = mole;
-        setTimeout(function () {currentCell.innerHTML = field}, interval);
-        }, 2000);
-    }
+        setTimeout(function () {
+            currentCell.innerHTML = field
+        }, interval);
+    }, 2000);
+}
 
 
 function gameLogic(table, mole, life, score, field, hit, miss) {
-
     let showTime = 1200;
     timedPopups(table, mole, field, showTime);
     for (let cell of table) {
@@ -53,12 +42,10 @@ function gameLogic(table, mole, life, score, field, hit, miss) {
             if (cell.innerHTML === mole) {
                 cell.innerHTML = hit;
                 score += 10;
-                //click_on_mole.play();
             } else {
                 cell.innerHTML = miss;
                 life -= 1;
                 score -= 10;
-                //missclick.play();
             }
             setTimeout(function () {
                 cell.innerHTML = field
@@ -69,7 +56,8 @@ function gameLogic(table, mole, life, score, field, hit, miss) {
     }
 }
 
-let background_sound,click_on_mole,missclick,death;
+let background_sound;
+
 function setup() {
     background_sound.setVolume(0.3);
     background_sound.loop();
@@ -79,12 +67,9 @@ function setup() {
 function preload() {
     soundFormats('mp3', 'ogg');
     background_sound = loadSound("/static/sounds/Free_SFX_Package/MP3/Music/Music-01.mp3");
-    //click_on_mole = loadSound("/static/sounds/FREE_SFX_Package/MP3/Input/Input-01.mp3");
-    //missclick = loadSound("/static/sounds/FREE_SFX_Package/MP3/Input/Input-04.mp3");
-    //death = loadSound("/static/sounds/FREE_SFX_Package/MP3/Alert/Alert-04.mp3")
 }
-function init() {
 
+function init() {
     let life = eval(document.querySelector('.life').textContent);
     let score = eval(document.querySelector('.scoring-system').textContent);
     let table = document.querySelectorAll('.col-md-1');
@@ -92,7 +77,6 @@ function init() {
     let emptyField = `<img alt="" src="/static/hill.png" class="imagepopup0">`;
     let registeredMiss = `<img alt="" src="/static/hillHit.png" class="imagepopup0">`;
     let registeredHit = `<img alt="" src="/static/hit.png" class="imagepopup0">`;
-
     gameLogic(table, mole, life, score, emptyField, registeredHit, registeredMiss);
 }
 
